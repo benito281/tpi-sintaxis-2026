04b29521e630b6750283f220015b9a0e1a2ba7c0
benito281 <benitoortiz281@gmail.com>
2026-05-13 22:35:50 -0300

    Lexer 1.0

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import tkinter as tk
 from colorama import Fore, init
 from tkinter import filedialog
 from interfaz import *
+from lexer import LexerTokens as tokens
+
+lexer = tokens() # Instancia de la clase LexerTokens
+lexer.build() # Construye el lexer
+
 
 class Ejecutable:
     def __init__(self):
         self.menu_de_opciones() # Ejecuta ni bien se instancia la clase
-        init() # Ayuda a camb
+        init() # Ayuda a cambiar de color las letras de la interfaz
         
     # Limpia la pantalla ni bien se la llame
     def limpiar_pantalla(self):
@@ -23,7 +28,7 @@ class Ejecutable:
         lineas = []
         try:
             while True:
-                linea = input("Línea: ")
+                linea = input(Fore.YELLOW+"Línea: ")
                 lineas.append(linea)
         
         except EOFError:
@@ -36,24 +41,36 @@ class Ejecutable:
             self.limpiar_pantalla()
             print(Fore.GREEN+BANNER_PRINCIPAL) #Muestra la interfaz de las opciones
             try:
-                entrada = input("Ingrese un numero para realizar una accion.\n")
+                print("Ingrese un numero para realizar una accion.")
+                entrada = input(Fore.YELLOW)
                 opcion = int(entrada)
-
+                print(Fore.GREEN)
                 if (opcion == 1):
                     self.limpiar_pantalla()
                     codigo_final = self.leer_datos_por_teclado()       # Guardamos lo que el usuario ingreso
-                    
-                    print("\n--- Código capturado con éxito ---")
-                    print(codigo_final) 
+                    print(Fore.GREEN)
+                    #print("\n--- Código capturado con éxito ---")
+                    #print(codigo_final) 
+                    lexer.test(codigo_final)
                     input("\nPresione Enter para volver al menú principal...")
 
                 elif (opcion == 2):
                     print("\nIngresar ubicación de archivo")
-                    self.leer_archivo()
-                    input("\nPresione Enter para volver al menú principal...")
+                    nombre_ubicacion = self.leer_archivo()
+                    if nombre_ubicacion:
+                        # Abrimos el archivo y leemos su contenido
+                        with open(nombre_ubicacion, 'r', encoding='utf-8') as archivo:
+                            codigo_archivo = archivo.read()
+                        
+                        lexer.test(codigo_archivo)
+                        input("\nPresione Enter para volver al menú principal...")
+                    else:
+                        print("No se introdujo el nombre de ningún archivo o este no existe.")
+                        input("\nPresione Enter para volver al menú principal...")
+
                 elif (opcion == 3):
                     self.limpiar_pantalla()
-                    print("\nSaliendo...")
+                    print(Fore.GREEN+"\nSaliendo...")
                     break
                 else:
                     self.limpiar_pantalla()
@@ -75,10 +92,11 @@ class Ejecutable:
         ruta_archivo = filedialog.askopenfilename(
             parent=root,
             title="Elija un archivo",
-            filetypes=[("Textos", "*.txt"), ("Todos", "*.*")]
+            filetypes=[("Textos", "*.txt"), ("SmartHome", "*.smart")]
         )
         root.destroy()
         print(ruta_archivo)
+        return ruta_archivo
 
 if __name__ == '__main__':
     ejecutar = Ejecutable()
